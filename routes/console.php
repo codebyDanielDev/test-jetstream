4f8b85f62bbf9a125167d7c8e01aa9d5be0749a0
Carlos Daniel Quispe Quispe <quispecarlosdaniel078@gmail.com>
2024-07-15 00:11:19 -0500

    test

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+// Tarea programada que se ejecuta cada minuto
+// Schedule::command('inspire')
+//         ->everyMinute()
+//         ->monitorName('Inspire Command')
+//         ->emailOutputTo('tardidaw@gmail.com');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
+
+// Schedule::command('app:test-notification')
+//         ->everyMinute()
+//         ->monitorName('test Command');
+
+// Programar el comando para que se ejecute cada minuto
+//app(Schedule::class)->command('app:test-notification')->everyMinute();
